5b97b33a6b0f5f7e25b07f2c78047da86b0af236
abubakarshf <18ntucs1050@student.ntu.edu.pk>
2021-08-18 12:06:59 +0500

    web-app

---
+++ web-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App=()=> {
+    return (
+        <React.Fragment>
+            <div>
+                <h1>
+                    App
+                </h1>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default App
